ef6132769d658cdce3004fc0133577c86b2a51d6
vladmatvienko <vematvienko@edu.hse.ru>
2023-05-31 21:25:17 +0300

    first commit

---
+++ Cards/src/main/java/com/example/sampleproject/CardController.java
@@ -0,0 +1,71 @@
+package com.example.sampleproject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+
+public class CardController {
+	Pane cardPane;
+	
+	GameWindowController gameWindow;
+
+    @FXML
+    private Text nominal;
+
+    @FXML
+    private Text mask;
+
+    @FXML
+    private ImageView imgBuffer;
+
+    @FXML
+    private Pane imgPane;
+
+   // @FXML
+   // private Text smth;
+
+    public void setCardParameters(String nominal, String mask, GameWindowController gameWindow, Pane cardPane) throws FileNotFoundException {
+    	this.nominal.setText(nominal);
+    	this.mask.setText(mask);
+    	this.gameWindow = gameWindow;
+    	this.cardPane = cardPane;
+    	
+    	File img = new File("C:\\Users\\vlado\\Desktop\\SampleProject\\target\\classes\\com\\example\\sampleproject\\EntrancePicture.jpg");
+    	InputStream isImage = (InputStream) new FileInputStream(img);
+    	imgBuffer = new ImageView(new Image(isImage));
+    	imgBuffer.setFitWidth(100);
+    	imgBuffer.setPreserveRatio(true);
+    	//imgBuffer.setSmooth(true);
+    	
+    	//cardPane.getChildren().add(imgBuffer);
+    	
+    	//imgPane = new Pane();
+    	imgPane.getChildren().setAll(imgBuffer);
+    	
+    }
+    
+    public String getNominal() {
+    	return this.nominal.getText();
+    }
+    
+    public String getMask() {
+    	return this.mask.getText();
+    }
+    
+    @FXML
+    void replaceCardToTable(MouseEvent event) throws IOException {
+    	gameWindow.addCardOnTable(this);
+    	cardPane.setVisible(false);
+    	//FlowPane firstPlayerPane = (FlowPane) cardPane.getParent();
+    	//firstPlayerPane.getChildren().remove(cardPane);
+    }
+}
---
+++ Cards/src/main/java/com/example/sampleproject/GameWindowController.java
@@ -0,0 +1,69 @@
+package com.example.sampleproject;
+
+import java.io.IOException;
+import java.util.Random;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+public class GameWindowController {
+
+    @FXML
+    private Button addCardButton;
+
+    @FXML
+    private GridPane deskAttackCardPane;
+
+    @FXML
+    private GridPane deskAnswerCardPane;
+    
+    @FXML
+    private ScrollPane firstPlayerScroll;
+
+    @FXML
+    private ScrollPane secondPlayerScroll;
+
+    @FXML
+    private FlowPane firstPlayerPane;
+
+    @FXML
+    private FlowPane secondPlayerPane;
+
+    @FXML
+    void addCard(ActionEvent event) throws IOException, InterruptedException {
+    	FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Card.fxml"));
+    	Pane newPane = (Pane)loader.load();
+        String[] nominal = {"6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+        String[] mask = {"бубны♦", "червы♥", "крести♣", "пики♠"};
+        Random random = new Random();
+        int index_1 = random.nextInt(nominal.length);
+        int index_2 = random.nextInt(mask.length);
+    	CardController cardController = loader.getController();
+    	cardController.setCardParameters(nominal[index_1], mask[index_2], this, newPane);
+
+
+
+    	firstPlayerPane.getChildren().add(newPane);
+
+    	firstPlayerScroll = new ScrollPane();
+    	firstPlayerScroll.setContent(firstPlayerPane);
+    }
+
+    public void addCardOnTable(CardController card) throws IOException {
+    	FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("Card.fxml"));
+        
+    	Pane newPane = (Pane)loader.load();
+    	
+    	CardController cardController = loader.getController();
+    	cardController.setCardParameters(card.getNominal(), card.getMask(), this, newPane);
+    	
+    	deskAttackCardPane.add(newPane, deskAttackCardPane.getChildren().size(), 0);
+    }
+}
