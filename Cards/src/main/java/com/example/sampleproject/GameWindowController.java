c3bbb780de8b25fbbf792a38636d8ec35cb3f2fb
vladmatvienko <vematvienko@edu.hse.ru>
2023-06-02 12:14:00 +0300

    Second commit

--- Cards/src/main/java/com/example/sampleproject/GameWindowController.java
+++ Cards/src/main/java/com/example/sampleproject/GameWindowController.java
@@ -1,6 +1,7 @@
 package com.example.sampleproject;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 
 import javafx.event.ActionEvent;
@@ -34,25 +35,40 @@ public class GameWindowController {
     @FXML
     private FlowPane secondPlayerPane;
 
+    public String[] MASSIVE = new String[6];// Массив карт которые добавляются в игру (Я не уверен, что оно работает, но мне по крайней мере одинаковые карты не попадались)
+    public int Num = 0;// Номер карты
     @FXML
     void addCard(ActionEvent event) throws IOException, InterruptedException {
     	FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("Card.fxml"));
     	Pane newPane = (Pane)loader.load();
-        String[] nominal = {"6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
-        String[] mask = {"Бубны♦", "Червы♥", "Крести♣", "Пики♠"};
-        Random random = new Random();
-        int index_1 = random.nextInt(nominal.length);
-        int index_2 = random.nextInt(mask.length);
-    	CardController cardController = loader.getController();
-    	cardController.setCardParameters(nominal[index_1], mask[index_2], this, newPane);
-
-
-
-    	firstPlayerPane.getChildren().add(newPane);
-
-    	firstPlayerScroll = new ScrollPane();
-    	firstPlayerScroll.setContent(firstPlayerPane);
+        int SomeNumber = 0;
+        while (SomeNumber==0) {
+            String[] nominal = new String[] {"6", "7", "8", "9", "10", "Валет", "Дама", "Король", "Туз"};
+            String[] mask = new String[] {"Бубны", "Червы", "Крести", "Пики"};
+            Random random = new Random();
+            int index_1 = random.nextInt(nominal.length);
+            int index_2 = random.nextInt(mask.length);
+            String OneCard = nominal[index_1] + mask[index_2];//Создание карты
+            int Num1 = 0;//Просто переменная
+            for (int k = 0; k <= 5; k++) {
+                if (MASSIVE[k] == OneCard) {
+                    Num1 = Num1+1;
+                    System.out.println(Num1);
+                }
+            }//Сравнивание карт в массиве и сгенерированной карты
+            if (Num1 == 0){
+                MASSIVE[Num] = OneCard;
+                Num = Num+1;
+                CardController cardController = loader.getController();
+                cardController.setCardParameters(nominal[index_1], mask[index_2], this, newPane);
+                firstPlayerPane.getChildren().add(newPane);
+                firstPlayerScroll = new ScrollPane();
+                firstPlayerScroll.setContent(firstPlayerPane);
+                SomeNumber = 1;
+            }//Если карта проходит по условию, то тогда добавляется в игру
+        }//Цикл сделал, что бы при нажатии кнопки по любому что-нибудь да впихнулось
+        System.out.println(Arrays.toString(MASSIVE));// это что б посмотреть что ваще в массив записалось
     }
 
     public void addCardOnTable(CardController card) throws IOException {
